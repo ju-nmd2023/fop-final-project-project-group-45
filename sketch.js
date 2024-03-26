2039e596cc326e276794587a868c1fa404539142
Wartburg101 <wartburgerik@outlook.com>
2024-03-26 15:44:53 +0100

    Planning

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let ball;
-
+let player = {health: 100, lives: 3, };
+//Ship object, 
 function setup() {
 	new Canvas(400, 700);
 
@@ -8,5 +9,18 @@ function setup() {
 }
 
 function draw() {
+	
 	background('gray');
+	//Main Menu Screen
+		//Shop
+		//Playbutton
+		//Settings
+	//Playscreen
+		//Level 1
+			//Waves
+		//Level 2...
 }
+
+//Make a playscreen, start coding level 1
+
+//Ship function restores health, etc.
