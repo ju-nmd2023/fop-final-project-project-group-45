f1e4a32c7259ec2c96fa2272c9e7c4ea4d23356f
MalteLindberg <lima23hn@student.ju.se>
2024-03-27 14:03:16 +0100

    test with sprites and backgrounds

--- js/main.js
+++ js/main.js
@@ -1,33 +1,37 @@
-let ball;
-let player = {health: 100, lives: 3 };
-//Ship object, 
+let player = { health: 100, lives: 3, sprite: "" };
+let playerBaseSprite;
+let testBackground;
+let y1 = -600;
+//Ship object,
+function preload() {
+  playerBaseSprite = loadImage("./assets/sprites/player/base_ship/base_ship_full_health.png");
+  testBackground = loadImage("./assets/backgrounds/space_background_test.png");
+}
 function setup() {
-	new Canvas(400, 700);
-	frameRate(30);
+  new Canvas(200, 350, "pixelated x2");
+  allSprites.pixelPerfect = true;
+  frameRate(30);
 
-	ball = new Sprite();
-	ball.diameter = 50;
+  player.sprite = new Sprite(32, 32, 32, 32);
+  player.sprite.img = playerBaseSprite;
 }
 
 function draw() {
-	
-	background('gray');
-
-	
-	//Main Menu Screen
-		//Shop
-		//Playbutton
-		//Settings
-	//Playscreen
-	playscreen();
-		//Level 1
-			//Waves
-		//Level 2...
+  clear();
+  image(testBackground, 0, y1, 200, 1000);
+  y1 += 1;
+  //Main Menu Screen
+  //Shop
+  //Playbutton
+  //Settings
+  //Playscreen
+  //playscreen();
+  //Level 1
+  //Waves
+  //Level 2...
 }
 
-function playscreen(){
-	
-}
+function playscreen() {}
 //Make a playscreen, start coding level 1
 
 //Ship function restores health, etc.
