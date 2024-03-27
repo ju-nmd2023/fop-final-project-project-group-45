b65707b97850aeb10651f43f33796813fd204ea4
Wartburg101 <wartburgerik@outlook.com>
2024-03-27 13:16:32 +0100

    changes in file structure

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 let ball;
-let player = {health: 100, lives: 3, };
+let player = {health: 100, lives: 3 };
 //Ship object, 
 function setup() {
 	new Canvas(400, 700);
+	frameRate(30);
 
 	ball = new Sprite();
 	ball.diameter = 50;
@@ -11,16 +12,22 @@ function setup() {
 function draw() {
 	
 	background('gray');
+
+	
 	//Main Menu Screen
 		//Shop
 		//Playbutton
 		//Settings
 	//Playscreen
+	playscreen();
 		//Level 1
 			//Waves
 		//Level 2...
 }
 
+function playscreen(){
+	
+}
 //Make a playscreen, start coding level 1
 
 //Ship function restores health, etc.
